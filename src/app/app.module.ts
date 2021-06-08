ace6aee6cb7f04fa9246f398448902e47bcb9e58
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-06-08 14:34:25 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { LikeButtonComponent } from './features/like-button/like-button.component';
+import { PokemonComponent } from './features/pokemon/pokemon.component';
+import { LogComponent } from './features/log/log.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LikeButtonComponent,
+    PokemonComponent,
+    LogComponent,
   ],
   imports: [
     BrowserModule,
