e34064b5e6f6743b162fc572c97d3cec794bfe04
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-06-08 15:09:27 +0200

    create two pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Pokemon} from './models/Pokemon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'PokeFight';
+  pokemon1: Pokemon;
+  pokemon2: Pokemon;
+
+
+  constructor() {
+    this.pokemon1 = new Pokemon({name: 'Pikachu', speed: 40, life: 60, attack: 12});
+    this.pokemon2 = new Pokemon({name: 'Bulbizarre', speed: 30, life: 100, attack: 10});
+  }
 }
--- src/app/utils/fight.ts
+++ src/app/utils/fight.ts
@@ -7,6 +7,14 @@ export function determinefirstAttacker(pok1: Pokemon, pok2: Pokemon, random = Ma
   return random() > 0.5 ? pok1 : pok2;
 }
 
+async function determineAttacker(pok1: Pokemon, pok2: Pokemon): Promise<Pokemon> {
+  if (isAnyPokemonDead(pok1, pok2)) {
+    throw new Error('One or both pokemon is / are dead so can\'t fight');
+  }
+
+  return determinefirstAttacker(pok1, pok2);
+}
+
 export function isAnyPokemonDead(pok1: Pokemon, pok2: Pokemon): boolean {
   return pok1.life === 0 || pok2.life === 0;
 }
@@ -32,11 +40,7 @@ export async function startAttackInterval(attacker: Pokemon, pok1: Pokemon, pok2
 }
 
 export async function fightArena(pok1: Pokemon, pok2: Pokemon, intervalMS = 1000, enableLog = true): Promise<Pokemon> {
-  if (isAnyPokemonDead(pok1, pok2)) {
-    throw new Error('One or both pokemon is / are dead so can\'t fight');
-  }
-
-  const attacker: Pokemon = await determinefirstAttacker(pok1, pok2);
+  const attacker = await determineAttacker(pok1, pok2);
 
   await startAttackInterval(attacker, pok1, pok2, intervalMS, enableLog);
   const winner: Pokemon = pok1.life > 0 ? pok1 : pok2;
