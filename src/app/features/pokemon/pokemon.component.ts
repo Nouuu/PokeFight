fad7b092765d117cb96e8f9258bcc8eede5b69eb
HERRERA Swann <swann@devloup.Dev>
2021-06-08 15:00:18 +0200

    add somes CSS

--- src/app/features/pokemon/pokemon.component.ts
+++ src/app/features/pokemon/pokemon.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.scss']
 })
 export class PokemonComponent implements OnInit {
+  hp: number;
+  maxHp: number;
+  hpPercents: number;
 
-  constructor() { }
+  constructor() {
+    this.hp = 80;
+    this.maxHp = 100;
+    this.hpPercents = this.hp / this.maxHp * 100;
+  }
 
   ngOnInit(): void {
   }
