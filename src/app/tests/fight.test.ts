62e5d083e62b3be6c6489b4fd34b570a07435f0a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-06-09 10:44:55 +0200

    fix fight tests

--- src/app/tests/fight.test.ts
+++ src/app/tests/fight.test.ts
@@ -1,7 +1,7 @@
 import {Pokemon} from '../models/Pokemon';
 import {FightService} from '../utils/fight.service';
-import {Logs} from '../models/BattleLog';
 import {TestBed} from '@angular/core/testing';
+import {LogService} from '../utils/log.service';
 
 
 const carapuce: Pokemon = new Pokemon({
@@ -25,26 +25,27 @@ const pikachu: Pokemon = new Pokemon({
 });
 // Pokemon { name: 'pikachu', speed: 90, attack: 55, life: 35 }
 
-const logs: Logs = new Logs();
 
 describe('Test determine pokemon first attacker function', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FightService]
+      providers: [FightService, LogService]
     });
   });
 
   describe('When pokemon don\'t have same speed', () => {
     it('should return Pikachu when Pikachu 90 speed attack Carapuce 43 speed', () => {
-      const arena: FightService = TestBed.get(FightService);
+      const arena: FightService = TestBed.inject(FightService);
       arena.setPaused(false);
-      expect(arena.determinefirstAttacker(pikachu, carapuce)).toBe(pikachu);
+      arena.setPokemons(pikachu, carapuce);
+      expect(arena.determinefirstAttacker()).toBe(pikachu);
     });
     it('should return Carapuce when Pikachu 90 speed attack Carapuce 100 speed', () => {
-      const arena: FightService = TestBed.get(FightService);
+      const arena: FightService = TestBed.inject(FightService);
       arena.setPaused(false);
+      arena.setPokemons(pikachu, carapuce);
       carapuce.speed = 100;
-      expect(arena.determinefirstAttacker(pikachu, carapuce)).toBe(carapuce);
+      expect(arena.determinefirstAttacker()).toBe(carapuce);
     });
   });
   describe('When pokemon have same speed', () => {
@@ -59,15 +60,17 @@ describe('Test determine pokemon first attacker function', () => {
       pikachu.speed = 90;
     });
     it('Should return pikachu (1st pokemon) when rand > 0.5', () => {
-      const arena: FightService = TestBed.get(FightService);
+      const arena: FightService = TestBed.inject(FightService);
       arena.setPaused(false);
-      expect(arena.determinefirstAttacker(pikachu, carapuce, randomMock)).toBe(pikachu);
+      arena.setPokemons(pikachu, carapuce);
+      expect(arena.determinefirstAttacker(randomMock)).toBe(pikachu);
     });
     it('Should return carapuce (2nd pokemon) when rand <= 0.5', () => {
-      const arena: FightService = TestBed.get(FightService);
+      const arena: FightService = TestBed.inject(FightService);
       arena.setPaused(false);
+      arena.setPokemons(pikachu, carapuce);
       randomMock = () => 0.5;
-      expect(arena.determinefirstAttacker(pikachu, carapuce, randomMock)).toBe(carapuce);
+      expect(arena.determinefirstAttacker(randomMock)).toBe(carapuce);
     });
   });
 });
@@ -76,7 +79,7 @@ describe('Test pokemon fight Arena function', () => {
   const mockIntervalMS = 10;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FightService]
+      providers: [FightService, LogService]
     });
   });
   afterEach(() => {
@@ -88,25 +91,28 @@ describe('Test pokemon fight Arena function', () => {
   });
 
   it('Should return as winner pikachu when carapuce has no chance', async () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     carapuce.currentLife = 1;
-    expect(await arena.fightArena(pikachu, carapuce, logs, mockIntervalMS)).toBe(pikachu);
+    expect(await arena.fightArena(mockIntervalMS)).toBe(pikachu);
   });
 
   it('Should return as winner carapuce when pikachu has no chance on long fight', async () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     carapuce.currentLife = 1000;
-    expect(await arena.fightArena(pikachu, carapuce, logs, mockIntervalMS, false)).toBe(carapuce);
+    expect(await arena.fightArena(mockIntervalMS, false)).toBe(carapuce);
   });
 
   it('Should throw error if one of pokemon is dead', async () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     carapuce.currentLife = 0;
     await expect(async () => {
-      await arena.fightArena(pikachu, carapuce, logs);
+      await arena.fightArena();
     }).rejects.toThrow('One or both pokemon is / are dead so can\'t fight');
   });
 
@@ -115,7 +121,7 @@ describe('Test pokemon fight Arena function', () => {
 describe('Test is any pokemon dead function', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FightService]
+      providers: [FightService, LogService]
     });
   });
   afterEach(() => {
@@ -127,37 +133,41 @@ describe('Test is any pokemon dead function', () => {
   });
 
   it('should return true if pikachu dead', () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     pikachu.currentLife = 0;
-    expect(arena.isAnyPokemonDead(pikachu, carapuce)).toBeTruthy();
+    expect(arena.isAnyPokemonDead()).toBeTruthy();
   });
 
   it('should return true if squirtle dead', () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     carapuce.currentLife = 0;
-    expect(arena.isAnyPokemonDead(pikachu, carapuce)).toBeTruthy();
+    expect(arena.isAnyPokemonDead()).toBeTruthy();
   });
 
   it('should return true if both are dead', () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     pikachu.currentLife = 0;
     carapuce.currentLife = 0;
-    expect(arena.isAnyPokemonDead(pikachu, carapuce)).toBeTruthy();
+    expect(arena.isAnyPokemonDead()).toBeTruthy();
   });
   it('should return false if both are alive', () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
-    expect(arena.isAnyPokemonDead(pikachu, carapuce)).toBeFalsy();
+    arena.setPokemons(pikachu, carapuce);
+    expect(arena.isAnyPokemonDead()).toBeFalsy();
   });
 });
 
 describe('Test attack interval function', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FightService]
+      providers: [FightService, LogService]
     });
   });
   afterEach(() => {
@@ -169,18 +179,20 @@ describe('Test attack interval function', () => {
   });
 
   it('should take squirtle to 0 hp when pikachu attack first', async () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     carapuce.currentLife = 10;
-    await arena.startAttackInterval(pikachu, pikachu, carapuce, 10, logs, false);
+    await arena.startAttackInterval(pikachu, 10, false);
     expect(carapuce.currentLife).toBe(0);
   });
 
   it('should take pikachu to 0 hp when squirtle has to many life attack first', async () => {
-    const arena: FightService = TestBed.get(FightService);
+    const arena: FightService = TestBed.inject(FightService);
     arena.setPaused(false);
+    arena.setPokemons(pikachu, carapuce);
     carapuce.currentLife = 3000;
-    await arena.startAttackInterval(pikachu, pikachu, carapuce, 10, logs, false);
+    await arena.startAttackInterval(pikachu, 10, false);
     expect(pikachu.currentLife).toBe(0);
   });
 });
