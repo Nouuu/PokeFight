fe0ee394d6de30f11eca9a6026e0403bb482ad04
Nouu <noelarrieulacoste@yahoo.fr>
2021-07-23 15:32:54 +0200

    search pokemon

--- src/app/features/pokeselect/list/list.component.ts
+++ src/app/features/pokeselect/list/list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Pokemon } from 'src/app/models/Pokemon';
-import { PokebuildService } from 'src/app/utils/pokebuild.service';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Pokemon} from 'src/app/models/Pokemon';
+import {PokebuildService} from 'src/app/utils/pokebuild.service';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-list',
@@ -9,29 +10,44 @@ import { PokebuildService } from 'src/app/utils/pokebuild.service';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  constructor(private pokebuild: PokebuildService) {}
+  constructor(private pokebuild: PokebuildService) {
+  }
+
   pokemons: Pokemon[] = [];
+  pokemonsFiltered: Pokemon[] = [];
   pokemonSelectioned: Pokemon[] = [];
+
+  searchInput = new FormControl('')
+
   ngOnInit(): void {
     this.setPokemons().subscribe(() => {
       console.log('ready');
     });
   }
+
   setPokemons(): Observable<void> {
     return new Observable((observer) => {
-      this.pokebuild.getPokelist(50).subscribe((pokeList) => {
+      this.pokebuild.getPokelist(151).subscribe((pokeList) => {
         this.pokemons = pokeList;
+        this.pokemonsFiltered = pokeList;
       });
     });
   }
 
   selectPokemon(index: number): void {
-    this.pokemonSelectioned.push(this.pokemons[index]);
+    this.pokemonSelectioned.push(this.pokemonsFiltered[index]);
   }
 
   removePokemon(removedPokemon: Pokemon) {
     this.pokemonSelectioned = this.pokemonSelectioned.filter(
       (poke) => poke != removedPokemon
     );
+  }
+
+  onSearch() {
+    console.log(this.searchInput.value);
+    const value = this.searchInput.value.toLowerCase();
+    this.pokemonsFiltered = this.pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().indexOf(value) >= 0)
   }
 }
