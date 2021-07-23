95d7bc46239871d174f2c6f06d4f873d963bc8ae
Nouu <noelarrieulacoste@yahoo.fr>
2021-07-23 14:21:22 +0200

    oad pokemon from url + handle pokemon not found

--- src/app/features/arena/arena.component.ts
+++ src/app/features/arena/arena.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { PokebuildService } from '../../utils/pokebuild.service';
-import { FightService } from '../../utils/fight.service';
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {PokebuildService} from '../../utils/pokebuild.service';
+import {FightService} from '../../utils/fight.service';
+import {Observable} from 'rxjs';
+import {ActivatedRoute, Router} from "@angular/router";
+
 @Component({
   selector: 'app-arena',
   templateUrl: './arena.component.html',
@@ -12,21 +14,26 @@ export class ArenaComponent implements OnInit {
 
   constructor(
     private pokebuild: PokebuildService,
-    public fightService: FightService
-  ) {}
+    public fightService: FightService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+  }
 
   async ngOnInit(): Promise<void> {
-    this.setPokemons().subscribe(() => {
+    const pokemon1 = this.route.snapshot.params.poke1;
+    const pokemon2 = this.route.snapshot.params.poke2;
+    this.setPokemons(pokemon1, pokemon2).subscribe(() => {
       console.log('ready');
       this.fightService.fightArena();
     });
     // this.fightService.fightArena();
   }
 
-  setPokemons(): Observable<void> {
+  setPokemons(pokemon1: string, pokemon2: string): Observable<void> {
     return new Observable((observer) => {
-      this.pokebuild.getPokemonFromPokedex('pikachu').subscribe((pok1) => {
-        this.pokebuild.getPokemonFromPokedex('eevee').subscribe((pok2) => {
+      this.pokebuild.getPokemonFromPokedex(pokemon1).subscribe((pok1) => {
+        this.pokebuild.getPokemonFromPokedex(pokemon2).subscribe((pok2) => {
           if (pok1 && pok2) {
             this.fightService.setPokemons(pok1, pok2);
             observer.next();
@@ -34,7 +41,11 @@ export class ArenaComponent implements OnInit {
           } else {
             throw new Error('Failed to build pokemons');
           }
+        }, err => {
+          this.router.navigate(['/']);
         });
+      }, err => {
+        this.router.navigate(['/']);
       });
     });
   }
